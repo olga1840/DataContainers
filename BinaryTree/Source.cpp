27349dcea1cdc5614d8c97e8b56201fe7c24e6c4
Rodch_ot15@top-academy.ru <Rodch_ot15@top-academy.ru>
2023-03-30 21:21:17 +0300

    Binary Metods

--- BinaryTree/Source.cpp
+++ BinaryTree/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class Tree
-{
+{protected:
 	class Element
 	{
 		int Data;
@@ -19,6 +19,7 @@ class Tree
 			cout << "EDestructor:\t" << this << endl;
 		}
 		friend class Tree;
+		friend class UniqueTree;
 	}*Root;
 public:
 	Element* getRoot()
@@ -35,6 +36,7 @@ public:
 	}
 	void insert(int Data, Element* Root)
 	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
 		if (Root == nullptr)return;
 		if (Data < Root->Data)
 		{
@@ -42,6 +44,64 @@ public:
 			else insert(Data, Root->pLeft);
 		}
 		else
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+
+	int minValue(Element* Root)
+	{
+		if (Root == nullptr)return 0;
+		/*if (Root->pLeft == nullptr)return Root->Data;
+		else return minValue(Root->pLeft);*/
+		return Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
+	}
+	int maxValue(Element* Root)
+	{
+		if (Root == nullptr)return 0;
+		return Root->pRight ? maxValue(Root->pRight) : Root->Data;
+	}
+
+	int Count(Element* Root)
+	{
+		if (Root == nullptr)return 0;
+		else return  Count(Root->pLeft) + Count(Root->pRight) + 1;
+	}
+
+	int Sum(Element* Root)
+	{
+		if (Root == nullptr)return 0;
+		else return  Sum(Root->pLeft) + Sum(Root->pRight)+Root->Data;
+	}
+	double Avg()
+	{
+		return (double)Sum(Root) / Count(Root);
+	}
+
+	void print(Element* Root)const
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << "\t";
+		print(Root->pRight);
+	}
+
+};
+
+class UniqueTree :public Tree
+{
+public:
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else if(Data>Root->Data)
 		{
 			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
 			else insert(Data, Root->pRight);
@@ -59,4 +119,24 @@ void main()
 	{
 		tree.insert(rand() % 100, tree.getRoot());
 	}
+	tree.print(tree.getRoot());
+	cout << endl;
+	cout << "Минимальное значение в дереве: " << tree.minValue(tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << tree.maxValue(tree.getRoot()) << endl;
+	cout << "Количество элементов дерева: " << tree.Count(tree.getRoot()) << endl;
+	cout << "Сумма элементов дерева: " << tree.Sum(tree.getRoot()) << endl;
+	cout << "Среднее арифметическое элементов дерева: " << tree.Avg() << endl;
+	UniqueTree tree2;
+	for (int i = 0; i < n; i++)
+	{
+		tree2.insert(rand() % 100, tree2.getRoot());
+	}
+	
+	tree2.print(tree2.getRoot());
+	cout << endl;
+	cout << "Минимальное значение в дереве: " << tree2.minValue(tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << tree2.maxValue(tree.getRoot()) << endl;
+	cout << "Количество элементов дерева: " << tree2.Count(tree.getRoot()) << endl;
+	cout << "Сумма элементов дерева: " << tree2.Sum(tree2.getRoot()) << endl;
+	cout << "Среднее арифметическое элементов дерева: " << tree2.Avg() << endl;
 }
